271cdbca799c7f9daaea17615a30a27fa513ca32
KingOfSpadesJFK <kingofspadeslm567@gmail.com>
2024-06-29 23:47:47 -0700

    creates nodes now

--- script/music_player.gd
+++ script/music_player.gd
@@ -1,13 +1,15 @@
 extends Node
 class_name MusicPlayer
 
-const PATH_TO_MUSIC: String = "res://asset/music"
+const PATH_TO_MUSIC: String = "res://asset/music/"
 const PATH_TO_TRACKLIST: String = "res://tracklist.json"
+const MUSIC_PLAYER_BUS: String = "Music"
+
 var tracklist: Dictionary
 
 class Track:
 	var name: String
-	var stream: Array[String]
+	var stream: Array
 
 
 func _ready():
@@ -20,6 +22,7 @@ func _ready():
 	if json.parse(content) == OK:
 		var data = json.data
 		var tracks = data["tracks"]
+		print(tracks)
 
 		# Loop through all the tracks and put them in the tracklist dictionary
 		for t in tracks:
@@ -30,8 +33,19 @@ func _ready():
 	else:
 		print("JSON Parse Error: ", json.get_error_message(), " in ", file, " at line ", json.get_error_line())
 
-	load_track("Regal")
+	#call_deferred("load_track", "Regal")
 
 
 func load_track(trackname: String) -> void:
-	pass
+	if tracklist.has(trackname):
+		var t: Track = tracklist[trackname]
+		var i = 0
+		for s in t.stream:
+			var asp: AudioStreamPlayer = AudioStreamPlayer.new()
+			asp.name = trackname + "#" + str(i)
+			asp.stream = load(PATH_TO_MUSIC + t.stream[i])
+			asp.bus = MUSIC_PLAYER_BUS
+			#asp.autoplay = true
+			add_child(asp)
+			i += 1
+		
