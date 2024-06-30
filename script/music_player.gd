6e873da2177558a67bfc2bc42e65a42dcd2dd222
KingOfSpadesJFK <kingofspadeslm567@gmail.com>
2024-06-30 00:50:38 -0700

    implemented volume scaling

--- script/music_player.gd
+++ script/music_player.gd
@@ -5,11 +5,26 @@ const PATH_TO_MUSIC: String = "res://asset/music/"
 const PATH_TO_TRACKLIST: String = "res://tracklist.json"
 const MUSIC_PLAYER_BUS: String = "Music"
 
-var tracklist: Dictionary
+# 0.0 --------------------- 1.0
+#  ^                         ^
+# Totally                  Fully
+#  Mute                   Audible
+const MAX_DB = 0.0
+const MIN_DB = -80.0
+
 
 class Track:
 	var name: String
 	var stream: Array
+
+
+### The tracklist for the current project
+var tracklist: Dictionary
+
+var _tracks_node: Node				# Use a node so that it can be easily cleared
+var _layer_count: int = 0
+var _volume: float = 1.0
+var _layer_volumes: Array[float]
 
 
 func _ready():
@@ -33,19 +48,34 @@ func _ready():
 	else:
 		print("JSON Parse Error: ", json.get_error_message(), " in ", file, " at line ", json.get_error_line())
 
-	#call_deferred("load_track", "Regal")
 
-
-func load_track(trackname: String) -> void:
+func load_track(trackname: String, volume: float = 1.0) -> void:
 	if tracklist.has(trackname):
+		# Clear the entire trackspace
+		if _tracks_node != null:
+			_tracks_node.name = "__goodbye__"
+			_tracks_node.queue_free()
+		_tracks_node = Node.new()
+
+		# Initialize private variables dedicated for playback
 		var t: Track = tracklist[trackname]
+		_tracks_node.name = t.name
+		_volume = volume
+		_layer_count = t.stream.size()
+		_layer_volumes.clear()
+		_layer_volumes.resize(_layer_count)
+		_layer_volumes.fill(1.0)
+
+		# Create the nodes of AudioStreamPlayers
 		var i = 0
 		for s in t.stream:
 			var asp: AudioStreamPlayer = AudioStreamPlayer.new()
 			asp.name = trackname + "#" + str(i)
 			asp.stream = load(PATH_TO_MUSIC + t.stream[i])
 			asp.bus = MUSIC_PLAYER_BUS
-			#asp.autoplay = true
-			add_child(asp)
+			asp.autoplay = true			# REMOVE THIS!!!
+			asp.volume_db = lerp(MIN_DB, MAX_DB, sqrt(_layer_volumes[i] * volume))
+			_tracks_node.add_child(asp)
 			i += 1
-		
+		add_child(_tracks_node)
+	return
