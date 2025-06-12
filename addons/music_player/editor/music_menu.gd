dc1f3b118ab9efc6a6cfa4751ce9dd73902fd20f
Miguel <kingofspadeslm567@gmail.com>
2025-06-12 15:06:28 -0700

    music playback

--- addons/music_player/editor/music_menu.gd
+++ addons/music_player/editor/music_menu.gd
@@ -9,6 +9,11 @@ var _current_track: String
 
 @export var _track_container: NodePath
 @export var _layer_container: NodePath
+@export var _play_button: NodePath
+@export var _stop_button: NodePath
+
+var _playing: bool = false
+var _first_play: bool = false
 
 
 func _enter_tree() -> void:
@@ -40,3 +45,32 @@ func _on_track_open(tn: String) -> void:
 	get_node(_layer_container).get_children().all(func (c): c.queue_free(); return true)
 	_load_layers(tn)
 	_current_track = tn
+
+	# Load the current track and stop playing the previous one
+	_music_player.load_track(_current_track, 1.0, false)
+	_on_stop_button_pressed()
+
+
+func _on_play_pause_button_pressed() -> void:
+	_toggle_play(!_playing)
+
+
+func _toggle_play(b: bool) -> void:
+	if _playing:
+		get_node(_play_button).text = "Play"
+		_music_player.pause()
+	else:
+		get_node(_play_button).text = "Pause"
+		if _first_play:
+			_music_player.pause()
+		else:
+			_first_play = true
+			_music_player.play()
+	_playing = b
+
+
+func _on_stop_button_pressed() -> void:
+	_first_play = false
+	get_node(_play_button).text = "Play"
+	_music_player.stop()
+	_playing = false
